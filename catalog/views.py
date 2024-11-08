81d56db696e222ba77eb6f3d04878e5516c5a12c
Виктор Бугай <ascon2913@gmail.com>
2024-11-08 21:37:12 +0500

    homework 26 done

--- catalog/views.py
+++ catalog/views.py
@@ -9,6 +9,7 @@ from django.views.generic import (
     UpdateView,
     DeleteView,
 )
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from catalog.forms import ProductForm
 from catalog.models import Product
@@ -18,7 +19,7 @@ class ProductListView(ListView):
     model = Product
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
 
 
@@ -26,19 +27,19 @@ class ContactsView(TemplateView):
     template_name = "catalog/contacts.html"
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy("catalog:product_list")
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy("catalog:product_list")
 
 
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     success_url = reverse_lazy("catalog:product_list")
 
---
+++ users/forms.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.forms import UserCreationForm
+
+from catalog.forms import StyleForm
+from users.models import User
+
+
+class UserRegisterForm(StyleForm, UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'phone_number', 'avatar', 'country', 'password1', 'password2')
